ff24c6d807fd9c36fd7191ebd9165882c84b7952
Younes Marzouk <younesmarzouk51@gmail.com>
2025-06-25 12:52:18 +0000

    Also, when navigating to any page in the app, at the top section where t

--- src/components/page-header.tsx
+++ src/components/page-header.tsx
@@ -1,4 +1,3 @@
-
 import * as React from 'react';
 
 type PageHeaderProps = {
@@ -8,8 +7,8 @@ type PageHeaderProps = {
 
 export default function PageHeader({ title, icon }: PageHeaderProps) {
   return (
-    <header className="bg-card p-4 shadow-md text-foreground sticky top-0 z-10 border-b">
-      <div className="flex items-center justify-center gap-3 text-primary">
+    <header className="bg-primary text-primary-foreground p-4 pb-8 rounded-b-[2rem] shadow-lg">
+      <div className="flex items-center justify-center gap-3">
         {icon}
         <h1 className="text-xl font-bold font-headline">{title}</h1>
       </div>
